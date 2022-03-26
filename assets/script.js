176021a2f35bd6bc817f6f5db34a3cfffab7f64c
Simona <simona.snapk@gmail.com>
2022-03-26 13:36:47 -0700

    linked files together, and also wrote some API notes in the js, and also set up a div framework in html

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+//the openweather API to use for homework
+//url: https://openweathermap.org
+
+//the API call for One Call API 1.0
+//the url to change: https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+//the API call for Geocoding API
+//the url to change: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+//the API key to use
+//02d9b2c5335979969e2585676e441daf
