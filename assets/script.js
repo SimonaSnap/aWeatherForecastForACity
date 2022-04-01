ec4340eda94a9f415c8b9c5561b6a28fbe4ca9b6
Simona <simona.snapk@gmail.com>
2022-03-31 23:15:46 -0700

    got the searh history to work, and also show future conditions!

--- assets/script.js
+++ assets/script.js
@@ -56,6 +56,114 @@ if (null != inStorage)
     searchHistory = inStorage.split(",")
 }
 
+
+function onCityBtnClick()
+{
+    var thisButton = document.getElementById(this.id);
+
+    var chosenCity = thisButton.textContent;
+
+    var getGeoURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + chosenCity + "&limit=3&appid=278839f9e7ae87dbb59691575959053f";
+
+    fetch(getGeoURL).then(function (response)
+    {
+        response.json().then(function (response)
+        {
+            var longitude = response[0].lon;
+            var latitude = response[0].lat;
+            var cityWeatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=278839f9e7ae87dbb59691575959053f";
+
+            fetch(cityWeatherURL).then(function (weather)
+            {
+                weather.json().then(function (weather)
+                {
+                    console.log(weather);
+                    var farenheit = ((weather.current.temp) - 273.15) * 1.8 + 32
+                    var roundFarenheit = farenheit.toFixed(1);
+                    currentForecast.hidden = false;
+
+                    cityName.textContent = chosenCity + "(" + day + ")" + " ";
+                    temp.textContent = "Temperature: " + roundFarenheit;
+                    wind.textContent = "Wind Speed: " + weather.current.wind_speed;
+                    humidity.textContent = "Humidity: " + weather.current.humidity;
+                    UVindex.textContent = "UV Index: " + weather.current.uvi;
+
+
+
+                    for (let i = 0; i < 5; i++)
+                    {
+                        var date = moment().add(i, "days").format("L");
+                        var displayDate = document.createElement("h3");
+                        var dayTemp = document.createElement("h4");
+                        var dayWind = document.createElement("h4");
+                        var dayHumidity = document.createElement("h4");
+                        var eachDay = document.createElement("div");
+                        eachDay.setAttribute("id", "future")
+
+                        var toFarenheit = ((weather.daily[i].temp.day) - 273.15) * 1.8 + 32
+                        var roundFarenheit = toFarenheit.toFixed(1);
+                        var description = weather.current.weather[0].main;
+
+                        eachDay.appendChild(displayDate);
+                        eachDay.appendChild(dayTemp);
+                        eachDay.appendChild(dayWind);
+                        eachDay.appendChild(dayHumidity);
+                        futureForecast.appendChild(eachDay);
+
+                        displayDate.textContent = date;
+                        dayTemp.textContent = "Temp: " + roundFarenheit;
+                        dayWind.textContent = "Wind: " + weather.daily[i].wind_speed;
+                        dayHumidity.textContent = "Humidity: " + weather.daily[i].humidity;
+                    }
+
+                    if (weather.current.uvi <= 2)
+                    {
+                        UVindex.style.backgroundColor = "lightgreen";
+                    }
+                    else if (weather.current.uvi <= 5)
+                    {
+                        UVindex.style.backgroundColor = "yellow";
+                    }
+                    else if (weather.current.uvi <= 7)
+                    {
+                        UVindex.style.backgroundColor = "lightsalmon";
+                    }
+                    else
+                    {
+                        UVindex.style.backgroundColor = "tomato";
+                    }
+
+                    if (description.indexOf("Clouds") >= 0)
+                    {
+                        cityName.append(cloudy);
+                    }
+                    else if (description.indexOf("Clear") >= 0)
+                    {
+                        cityName.append(sunnyclear);
+                    }
+                    else if (description.indexOf("Rain") >= 0)
+                    {
+                        cityName.append(raining);
+                    }
+                    else if (description.indexOf("Snow") >= 0)
+                    {
+                        cityName.append(snowing);
+                    }
+                    else if (description.indexOf("Haze") >= 0)
+                    {
+                        cityName.append(haze);
+                    }
+                    else if (description.indexOf("Thunderstorm") >= 0)
+                    {
+                        cityName.append(storm);
+                    }
+
+                })
+            })
+        })
+    })
+}
+
 for (let i = 0; i < searchHistory.length; i++)
 {
     var cityBtn = document.createElement("button");
@@ -63,132 +171,10 @@ for (let i = 0; i < searchHistory.length; i++)
     cityBtn.textContent = searchHistory[i];
     searched.appendChild(cityBtn);
     cityBtn.setAttribute("id", i.toString());
+    //don't put parameter parantheses after the function - it means the function is being run, and you want it to be called on click
+    cityBtn.addEventListener("click", onCityBtnClick);
 }
 
-function onCityBtnClick()
-{
-    var btnCity = this.value;
-    var geoCodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + btnCity + "&limit=3&appid=278839f9e7ae87dbb59691575959053f";
-
-    var cityHistory = [];
-    var savedCities = localStorage.getItem("city");
-    if (null != savedCities)
-    {
-        cityHistory = savedCities.split(",");
-    }
-
-
-    if (city === "")
-    {
-        citySearch.textContent = "Please search for a city";
-    }
-    else
-    {
-        cityHistory.push(city);
-        localStorage.setItem("city", cityHistory.toString());
-
-        fetch(geoCodeURL).then((function (response)
-        {
-            response.json().then(function (response)
-            {
-                //console.log(response);
-                var longitude = response[0].lon;
-                var latitude = response[0].lat;
-                var cityWeatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&appid=278839f9e7ae87dbb59691575959053f";
-
-                fetch(cityWeatherURL).then(function (weather)
-                {
-                    weather.json().then(function (weather)
-                    {
-                        console.log(weather);
-                        var farenheit = ((weather.current.temp) - 273.15) * 1.8 + 32
-                        var roundFarenheit = farenheit.toFixed(1);
-                        currentForecast.hidden = false;
-
-                        cityName.textContent = city + "(" + day + ")" + " ";
-                        temp.textContent = "Temperature: " + roundFarenheit;
-                        wind.textContent = "Wind Speed: " + weather.current.wind_speed;
-                        humidity.textContent = "Humidity: " + weather.current.humidity;
-                        UVindex.textContent = "UV Index: " + weather.current.uvi;
-
-
-
-                        for (let i = 0; i < 5; i++)
-                        {
-                            var date = moment().add(i, "days").format("L");
-                            var displayDate = document.createElement("h3");
-                            var dayTemp = document.createElement("h4");
-                            var dayWind = document.createElement("h4");
-                            var dayHumidity = document.createElement("h4");
-                            var eachDay = document.createElement("div");
-                            eachDay.setAttribute("id", "future")
-
-                            var toFarenheit = ((weather.daily[i].temp.day) - 273.15) * 1.8 + 32
-                            var roundFarenheit = toFarenheit.toFixed(1);
-                            var description = weather.current.weather[0].main;
-
-                            eachDay.appendChild(displayDate);
-                            eachDay.appendChild(dayTemp);
-                            eachDay.appendChild(dayWind);
-                            eachDay.appendChild(dayHumidity);
-                            futureForecast.appendChild(eachDay);
-
-                            displayDate.textContent = date;
-                            dayTemp.textContent = "Temp: " + roundFarenheit;
-                            dayWind.textContent = "Wind: " + weather.daily[i].wind_speed;
-                            dayHumidity.textContent = "Humidity: " + weather.daily[i].humidity;
-                        }
-
-                        if (weather.current.uvi <= 2)
-                        {
-                            UVindex.style.backgroundColor = "lightgreen";
-                        }
-                        else if (weather.current.uvi <= 5)
-                        {
-                            UVindex.style.backgroundColor = "yellow";
-                        }
-                        else if (weather.current.uvi <= 7)
-                        {
-                            UVindex.style.backgroundColor = "lightsalmon";
-                        }
-                        else
-                        {
-                            UVindex.style.backgroundColor = "tomato";
-                        }
-
-                        if (description.indexOf("Clouds") >= 0)
-                        {
-                            cityName.append(cloudy);
-                        }
-                        else if (description.indexOf("Clear") >= 0)
-                        {
-                            cityName.append(sunnyclear);
-                        }
-                        else if (description.indexOf("Rain") >= 0)
-                        {
-                            cityName.append(raining);
-                        }
-                        else if (description.indexOf("Snow") >= 0)
-                        {
-                            cityName.append(snowing);
-                        }
-                        else if (description.indexOf("Haze") >= 0)
-                        {
-                            cityName.append(haze);
-                        }
-                        else if (description.indexOf("Thunderstorm") >= 0)
-                        {
-                            cityName.append(storm);
-                        }
-
-                    })
-                })
-            })
-        }))
-    }
-}
-
-cityBtn.addEventListener("click", onCityBtnClick());
 
 
 submitCity.addEventListener("click", function (event)
